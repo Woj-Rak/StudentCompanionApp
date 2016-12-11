eb108e3a33b609a600e544a6efdde97cb1aec9d1
Wojtek Rak <fallenzerg@gmail.com>
2016-12-11 13:03:45 +0000

    Map Fragment now displays inside the Locator Tab

--- app/src/main/java/com/example/wojtek/studentcompanion/Fragments/LocatorFragment.java
+++ app/src/main/java/com/example/wojtek/studentcompanion/Fragments/LocatorFragment.java
@@ -2,17 +2,30 @@ package com.example.wojtek.studentcompanion.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.wojtek.studentcompanion.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
 
 /**
  * Created by Wojtek on 29/11/2016.
  */
-public class LocatorFragment extends Fragment {
+public class LocatorFragment extends Fragment implements OnMapReadyCallback{
+
+    private SupportMapFragment fragment;
+    private GoogleMap map;
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -40,4 +53,19 @@ public class LocatorFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_locator, container, false);
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment fragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+
+    }
+
+
 }
